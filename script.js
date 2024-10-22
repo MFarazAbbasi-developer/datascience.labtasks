4cd966d0486650c738707d4cb572ec51161f7e9e
MFarazAbbasi-developer <muhammadfaraz.abbasi786@gmail.com>
2024-10-22 12:06:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn")
+let heading = document.getElementById("heading")
+
+
+btn.addEventListener("click", ()=>{
+    heading.style.display = "block"
+    btn.style.display = "none"
+    
+})
